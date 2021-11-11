80d1c3732a9a603b0e2908be8f2d62f8c38ce611
Tomaz <tomazkunst@gmail.com>
2021-11-11 21:43:18 +0100

    added registration and login

--- glasljudstva/zahteve/urls.py
+++ glasljudstva/zahteve/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
-from zahteve.views import landing, delovna_skupina
+from zahteve.views import landing, delovna_skupina, Registracija
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', landing),
     path('<int:delovna_skupina_id>/', delovna_skupina),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+
+    path('registracija/', Registracija.as_view()),
 ]
