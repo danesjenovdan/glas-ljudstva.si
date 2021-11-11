cdab89a14396aeba42f69a2c93f4fecdc23725d5
patricijab <patricija.brecko@gmail.com>
2021-11-12 00:04:23 +0100

    frontend fixes and zahteva page

--- glasljudstva/zahteve/urls.py
+++ glasljudstva/zahteve/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from zahteve.views import landing, delovna_skupina
+from zahteve.views import landing, delovna_skupina, demand
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', landing),
     path('<int:delovna_skupina_id>/', delovna_skupina),
+    path('zahteve/<int:demand_id>/', demand),
 ]
--- glasljudstva/zahteve/views.py
+++ glasljudstva/zahteve/views.py
@@ -5,8 +5,10 @@ from zahteve.models import WorkGroup, Demand
 
 # Create your views here.
 def landing(request):
-    demands = Demand.objects.all()
-    return render(request, 'zahteve/landing.html', context={'demands': demands})
+    work_groups = WorkGroup.objects.all()
+    for wg in work_groups:
+        wg.demands = Demand.objects.filter(workgroup=wg)
+    return render(request, 'zahteve/landing.html', context={'work_groups': work_groups})
 
 def delovna_skupina(request, delovna_skupina_id):
     try:
@@ -16,4 +18,12 @@ def delovna_skupina(request, delovna_skupina_id):
     
     demands = Demand.objects.filter(workgroup=delovna_skupina)
 
-    return render(request, 'zahteve/delovna_skupina.html', context={'demands': demands, 'id': delovna_skupina_id})
+    return render(request, 'zahteve/delovna_skupina.html', context={'demands': demands, 'delovna_skupina': delovna_skupina})
+
+def demand(request, demand_id):
+    try:
+        demand = Demand.objects.get(id=demand_id)
+    except:
+        Demand.HttpResponseNotFound()
+
+    return render(request, 'zahteve/zahteva.html', context={'demand': demand})
