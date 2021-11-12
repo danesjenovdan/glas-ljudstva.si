be1793a3084700716a5b9c64668815498a880de4
muki <filipdobranic@gmail.com>
2021-11-12 15:38:04 +0100

    some S3-related fixes

--- glasljudstva/glasljudstva/k8s_settings.py
+++ glasljudstva/glasljudstva/k8s_settings.py
@@ -182,7 +182,7 @@ if os.getenv('DJANGO_ENABLE_S3', False):
     AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME', 'djnd')
     AWS_DEFAULT_ACL = 'public-read' # if files are not public they won't show up for end users
     AWS_QUERYSTRING_AUTH = False # query strings expire and don't play nice with the cache
-    AWS_LOCATION = os.getenv('DJANGO_AWS_LOCATION', 'lestvica-transparentnosti')
+    AWS_LOCATION = os.getenv('DJANGO_AWS_LOCATION', 'glas-ljudstva')
     AWS_S3_REGION_NAME = os.getenv('DJANGO_AWS_REGION_NAME', 'fr-par')
     AWS_S3_ENDPOINT_URL = os.getenv('DJANGO_AWS_S3_ENDPOINT_URL', 'https://s3.fr-par.scw.cloud')
     AWS_S3_SIGNATURE_VERSION = os.getenv('DJANGO_AWS_S3_SIGNATURE_VERSION', 's3v4')
