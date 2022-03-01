de71d3875184f9eed3cbd58a764057ac9fdf7dda
patricijab <patricija.brecko@gmail.com>
2022-03-01 13:19:21 +0100

    change party serializer image field

--- glasljudstva/zahteve/serializers.py
+++ glasljudstva/zahteve/serializers.py
@@ -3,6 +3,12 @@ from .models import Party
 
 class PartySerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='user.id')
+    image_url = serializers.SerializerMethodField('get_image_url')
     class Meta:
         model = Party
-        fields = ['id', 'party_name', 'image', 'url']
+        fields = ['id', 'party_name', 'image_url', 'url']
+    def get_image_url(self, obj):
+        if obj.image:
+            return obj.image.url
+        else:
+            return ''
